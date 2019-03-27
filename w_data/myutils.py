ee94d02be4f1979844d8ec05901dd1dc04dfec3b
wenqinlong <wenqinlong@126.com>
2019-03-27 18:53:33 -0400

    first version 30/27/2019

---
+++ w_data/myutils.py
@@ -0,0 +1,63 @@
+import tensorflow as tf
+
+
+def fc(x, units):
+    x = tf.layers.dense(inputs=x, units=units)
+    return x
+
+
+def conv(x, f, w):
+    x = tf.layers.conv2d(inputs=x, filters=f, kernel_size=w, strides=1, padding='same')
+    return x
+
+
+def t_conv2d(x, f, w, s):
+    x = tf.layers.conv2d_transpose(x, filters=f, kernel_size=w, strides=s, padding='same')
+    return x
+
+
+def max_pool(x, ps, s):
+    x = tf.layers.max_pooling2d(inputs=x, pool_size=ps, stides=s)
+    return x
+
+
+def bn(x, training=True):
+    x = tf.layers.batch_normalization(inputs=x, axis=-1, momentum=0.99, epsilon=1e-3, training=training)
+    return x
+
+
+def activation(x, relu=True):
+    if relu:
+        x = tf.nn.relu(x, name='relu')
+    else:
+        x = tf.nn.tanh(x)
+    return x
+
+
+def data_norm(x, para, reverse=False):
+    if para == 'top_length':
+        max = 1600
+        min = 1000
+    elif para == 'bottom_length':
+        max = 1600
+        min = 1000
+    elif para == 'top_spacer':
+        max = 600
+        min = 200
+    elif para == 'bottom_spacer':
+        max = 600
+        min = 200
+    else:                           # para = 'angle'
+        max = 180
+        min = 0
+
+    if not reverse:
+        x = 2.0 * (x - min) / (max - min) - 1   # confine the data to range [-1, 1]
+    else:
+        x = 0.5 * (x + 1) * (max - min) + min
+    return x
+
+
+def mse_loss(y, y_):
+    loss = tf.losses.mean_squared_error(y, y_)
+    return loss
---
+++ w_data/tfrecord.py
@@ -0,0 +1,77 @@
+import os
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+
+def tfrecord(path, out):
+    writer = tf.python_io.TFRecordWriter(out)
+    file = []
+    for i in os.listdir(path):
+        file.append(i)
+    file.sort()
+    print(file)
+    LL_norm = pd.read_csv(path+file[0])
+    LR_norm = pd.read_csv(path+file[1])
+    RR_norm = pd.read_csv(path+file[2])
+    for index in LL_norm.index.values:
+        label = LL_norm.loc[index][0]
+        para = np.array(LL_norm.loc[index][1:6])
+        spec_LL = LL_norm.loc[index][6:]
+        spec_LR = LR_norm.loc[index][6:]
+        spec_RR = RR_norm.loc[index][6:]
+        spec = np.concatenate([spec_LL, spec_LR, spec_RR], axis=0)
+
+        example = tf.train.Example(features=tf.train.Features(feature={                          # tf.train.Example
+            'para': tf.train.Feature(float_list=tf.train.FloatList(value=[i for i in para])),    # float
+            'label': tf.train.Feature(float_list=tf.train.FloatList(value=[label])),
+            'spec': tf.train.Feature(float_list=tf.train.FloatList(value=[i for i in spec]))
+        }))
+
+        writer.write(example.SerializeToString())  # Don't forget the ()
+    writer.close()
+    return out
+
+
+def read_tfrecord(fn, batch_size):
+    dataset = tf.data.TFRecordDataset(fn)
+
+    def parser(record):
+        features = {
+            'para': tf.FixedLenFeature([5], tf.float32),
+            'label': tf.FixedLenFeature([1], tf.float32),
+            'spec': tf.FixedLenFeature([603], tf.float32)    # 加上具体的维度数据  否则报错 Can't parse serialized Example
+        }
+        parsed = tf.parse_single_example(record, features)
+        para = parsed['para']
+        label = parsed['label']
+        spec = parsed['spec']
+
+        return para, label, spec
+
+    dataset = dataset.map(parser)
+    dataset = dataset.shuffle(buffer_size=100000)
+    dataset = dataset.batch(batch_size, drop_remainder=True)
+    dataset = dataset.repeat()
+    iterator = dataset.make_one_shot_iterator()
+    data = iterator.get_next()
+
+    return data
+
+
+if __name__ == '__main__':
+    CWD = '/home/qinlong/PycharmProjects/NEU/wei_data_practice/'
+    # tfrecord(CWD+'data/test_norm/', 'test_data.tfrecord')
+    # tfrecord(CWD+'data/train_norm/', 'train_data.tfrecord')
+
+    para_batch, label_batch, spec_batch = read_tfrecord(CWD + 'test_data.tfrecord', 2)
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        for i in range(5):
+            p, l, s = sess.run([para_batch, label_batch, spec_batch])
+            #para_batch, label_batch, spec_batch = sess.run([para_batch, label_batch, spec_batch])
+            print(p, l, s)
