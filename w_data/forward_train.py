50f63f79470f65395029f21a08979836fed54e04
qinlong <https://github.com/wenqinlong/w_b_transfer.git>
2019-03-28 23:37:21 -0400

    add tensorboard and unfinished saved_model

--- w_data/forward_train.py
+++ w_data/forward_train.py
@@ -21,8 +21,7 @@ test_dataset = tfrecord.read_tfrecord(cwd + 'train_data.tfrecord', BATCH_SIZE)
 # loss
 r_spec = tf.placeholder(tf.float32, shape=[BATCH_SIZE, 603], name='r_spec')
 p_spec = fn.fw_net(x_para)
-train_loss = ms.huber_loss(r_spec, p_spec)
-test_loss = ms.huber_loss(r_spec, p_spec)
+loss = ms.huber_loss(r_spec, p_spec)
 
 # optimizer
 optimizer = tf.train.AdamOptimizer(learning_rate=LR,
@@ -31,31 +30,33 @@ optimizer = tf.train.AdamOptimizer(learning_rate=LR,
                                    epsilon=1e-8)
 
 # minimize
-fw_op = optimizer.minimize(loss=train_loss)
+fw_op = optimizer.minimize(loss=loss)
 
 # start training
 init = tf.global_variables_initializer()
 
-tf.summary.scalar('Train loss', train_loss)
-tf.summary.scalar('Test loss', test_loss)
-merged_summary = tf.summary.merge_all()
+train_summary = tf.summary.scalar('Train loss', loss)
+test_summary = tf.summary.scalar('Test loss', loss)
+# merged_summary = tf.summary.merge_all()  # if want the train and test loss in the same image, then uncomment this line
 
 config = tf.ConfigProto()
 config.gpu_options.per_process_gpu_memory_fraction = 0.8
 
+builder = tf.saved_model.builder.SavedModelBuilder('./model')
+
 with tf.Session(config=config) as sess:
     sess.run(init)
 
-    writer = tf.summary.FileWriter('./gpaphs', tf.get_default_graph())
-    writer.add_graph(sess.graph)
+    train_writer = tf.summary.FileWriter('./graphs/train', tf.get_default_graph())
+    test_writer = tf.summary.FileWriter('./graphs/test')
 
     epoch = 0
 
     for it in range(EPOCH*(30513//BATCH_SIZE)):
         pa_tr, id_tr, sp_tr = sess.run([para_train, id_train, sp_train])
 
-        summary_tr, _, l = sess.run([merged_summary, fw_op, train_loss], feed_dict={x_para: pa_tr, r_spec: sp_tr})  # _, loss = sess.run([fw_op, loss], feed_dict={x_para: pa, r_spec: sp})
-        writer.add_summary(summary_tr, it)
+        summary_tr, _, l = sess.run([train_summary, fw_op, loss], feed_dict={x_para: pa_tr, r_spec: sp_tr})  # _, loss = sess.run([fw_op, loss], feed_dict={x_para: pa, r_spec: sp})
+        train_writer.add_summary(summary_tr, it)
 
         if (it+1) % 238 == 0:
             epoch += 1
@@ -63,13 +64,24 @@ with tf.Session(config=config) as sess:
 
     for i in range(5000//64):
         pa_te, id_te, sp_te = sess.run([para_test, id_test, sp_test])
-        summary_te, l_test = sess.run([merged_summary, test_loss], feed_dict={x_para: pa_te, r_spec: sp_te})
-        writer.add_summary(summary_te, i)
 
-        if (i+1) % 100 == 0:
+        p_spec_test, summary_te, l_test = sess.run([p_spec, test_summary, loss],
+                                                   feed_dict={x_para: pa_te, r_spec: sp_te})
+        test_writer.add_summary(summary_te, i)
+
+        if (i+1) % 10 == 0:
             print('Iteration_{}, loss: {}'.format(i, l_test))
 
-writer.close()
+    # save the model
 
+    builder.add_meta_graph(
+        sess,
+        [tf.saved_model.tag_constants.SERVING],
+    )
+    builder.save()
+
+
+train_writer.close()
+test_writer.close()
 # tensorboard --logdir='/home/qinlong/PycharmProjects/NEU/w_b_transfer/w_data' --port=8008
 # Don't enter the graphs folder, don't add spaces before and after '='
